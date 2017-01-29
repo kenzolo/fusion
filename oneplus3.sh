2c5651c990ce85cfb872b684b318183e37acc955
kenzolo <vincenzo.giovinazzo91@gmail.com>
2017-01-29 15:53:42 +0100

    BUild script

---
+++ oneplus3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+. bbuild-anykernel.sh 0 &&
+. bbuild-anykernel.sh 1 &&
+. bbuild-anykernel.sh 2 &&
+. bbuild-anykernel.sh 3 &&
+. bbuild-anykernel.sh 4 &&
+. bbuild-anykernel.sh 5 
+
+
+
+
+
+
