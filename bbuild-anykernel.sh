2f6ed30b6ef0a946ea2c284c1ddc77707833c57c
kenzolo <vincenzo.giovinazzo91@gmail.com>
2017-03-09 19:56:22 +0100

    Fusion 2.5

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -11,7 +11,7 @@
 #######################################
 
 
-BOEFFLA_VERSION="UNOFFICIAL-Fusion-2.4"
+BOEFFLA_VERSION="UNOFFICIAL-Fusion-2.5"
 
 TOOLCHAIN="/home/kenzolo/Rom/tool/aarch64-linux-android-6.x/bin/aarch64-linux-android-"
 ARCHITECTURE=arm64
