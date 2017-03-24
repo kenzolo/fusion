38423265fff5983157e7c0829cc6dd3c640c92af
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-24 01:34:33 +0000

    signal: remove stupid OEM log spam
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -1031,6 +1031,7 @@ static int __send_signal(int sig, struct siginfo *info, struct task_struct *t,
 	assert_spin_locked(&t->sighand->siglock);
 
 	result = TRACE_SIGNAL_IGNORED;
+
 	if (!prepare_signal(sig, t,
 			from_ancestor_ns || (info == SEND_SIG_FORCED)))
 		goto ret;
