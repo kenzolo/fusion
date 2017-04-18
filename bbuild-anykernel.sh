5a0880f3a7b57b71f6bd00dd159132d90d6b2dd4
andip71 <andreasp@gmx.de>
2017-04-18 10:03:33 +0200

    misc: fpc1020: Ignore home key presses when touchscreen is in use All credits to Sultanxda <sultanxda@gmail.com>
    When the user's finger is on the touchscreen, don't process any home key
    button presses, as an unintended button press will cause poor UX.
    
    Ignore home key presses when the Synaptics s1302 driver is ignoring keypad
    presses.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>
    Signed-off-by: flar2 <asegaert@gmail.com>

--- bbuild-anykernel.sh
+++ bbuild-anykernel.sh
@@ -11,7 +11,7 @@
 #######################################
 
 
-BOEFFLA_VERSION="UNOFFICIAL-Fusion-3.2"
+BOEFFLA_VERSION="UNOFFICIAL-Fusion-3.3"
 
 TOOLCHAIN="/home/kenzolo/Rom/tool/aarch64-linux-android-6.x/bin/aarch64-linux-android-"
 ARCHITECTURE=arm64
